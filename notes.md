2e55eaee312bb141b115b74cd6b1881385b2ca7f
Dylane <tanolamene@gmail.com>
2025-01-20 19:58:29 -0400

    add bird module with controller and service; update app module to include bird module

--- notes.md
+++ notes.md
@@ -1,5 +1,10 @@
 L'objet request est composé du Body, des query paramètres, des paths paramètres et des headers.
 Le Service agit comme une interface entre les endpoints de l'API et la base de données, en encapsulant la logique métier.
 
+Le module est le liant entre controllers et providers 
+le lien qui facilite l'injection de dépendance entre controller et provider
+
 Injections de depence
 // provider et injecteur
+
+le provider peut etre injecter quelque par
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DogController } from './dog/dog.controller';
 import { DogService } from './dog/dog.service';
+import { BirdModule } from './bird/bird.module';
 
 @Module({
-  imports: [],
+  imports: [BirdModule],
   controllers: [AppController, DogController],
   // renseigne tout mes providers definie dans les fichier .services.ts
   providers: [AppService, DogService],
